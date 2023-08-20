5032e6ed6ae0c5a5ba07a718b3588e354cd60b3a
Gabriel Martini <gabrielb.martini@gmail.com>
2023-08-20 13:39:04 -0300

    feat: s3 sensor

--- dags/ml-pipeline.py
+++ dags/ml-pipeline.py
@@ -1,50 +1,16 @@
 from datetime import datetime, timedelta
-import logging
 
 from airflow import DAG
 from airflow.models import Variable
 
-from airflow.hooks.S3_hook import S3Hook
-
-from airflow.operators.python_operator import PythonVirtualenvOperator
+from airflow.operators.sensors import S3KeySensor
 from airflow.operators.python import PythonOperator
 
 from train import train
 from serve import serve
 
-
-def list_keys():
-    hook = S3Hook(aws_conn_id="aws_credentials")
-    bucket = Variable.get("s3_bucket")
-    prefix = Variable.get("s3_prefix")
-    logging.info(f"Listing Keys from {bucket}/{prefix}")
-    keys = hook.list_keys(bucket, prefix=prefix)
-    logging.info(keys)
-    return keys
-
-
-# def train_ray():
-#     from ray.job_submission import JobSubmissionClient
-
-#     client = JobSubmissionClient(
-#         "http://ray-cluster-kuberay-head-svc.ray-cluster.svc.cluster.local:8265"
-#     )
-
-#     # Submit job to Ray Cluster
-#     kick_off_gpt2_training = (
-#         "rm -rf finetune-gpt2-ray-test && git clone https://github.com/lusoal/finetune-gpt2-ray-test || true;"
-#         " chmod +x finetune-gpt2-ray-test/llm-distributed-fine-tunning.py && python finetune-gpt2-ray-test/llm-distributed-fine-tunning.py --model='tiiuae/falcon-7b' --num-workers 4"
-#     )
-
-#     print("training")
-#     submission_id = client.submit_job(
-#         entrypoint=kick_off_gpt2_training,
-#     )
-#     print("Training: " + submission_id)
-
-
 default_args = {
-    "owner": "martinig",
+    "owner": "fmops",
     "depends_on_past": False,
     "start_date": datetime(2023, 8, 18),
     "retries": 0,
@@ -54,17 +20,31 @@ dag = DAG(
     "ml-pipeline",
     default_args=default_args,
     dagrun_timeout=timedelta(hours=1),
-    schedule_interval="0 3 * * *",
+    schedule_interval="@daily",
 )
 
-# train_task = PythonVirtualenvOperator(
-#     task_id="train", requirements="ray", python_callable=train_ray, dag=dag
-# )
+bucket = Variable.get("s3_bucket")
+prefix = Variable.get("s3_prefix")
 
-s3_task = PythonOperator(task_id="dataset", python_callable=list_keys, dag=dag)
+s3_sensor = S3KeySensor(
+    task_id="s3_file_check",
+    poke_interval=60,
+    timeout=180,
+    soft_fail=False,
+    retries=2,
+    bucket_key=prefix,
+    bucket_name=bucket,
+    aws_conn_id="s3_connection",
+    dag=dag,
+)
 
-train_task = PythonOperator(task_id="train", python_callable=train, dag=dag)
+train_task = PythonOperator(
+    task_id="train",
+    python_callable=train,
+    op_kwargs={"bucket": bucket, "prefix": prefix},
+    dag=dag,
+)
 
 serve_task = PythonOperator(task_id="serve", python_callable=serve, dag=dag)
 
-s3_task >> train_task >> serve_task
+s3_sensor >> train_task >> serve_task
--- dags/train.py
+++ dags/train.py
@@ -1,2 +1,14 @@
-def train():
-    print("training")
+import logging
+from sys import prefix
+from airflow.hooks.S3_hook import S3Hook
+
+
+def train(**kwargs):
+    s3_hook = S3Hook(aws_conn_id="s3_connection")
+    bucket = kwargs["bucket"]
+    prefix = kwargs["prefix"]
+
+    keys = s3_hook.list_keys(bucket, prefix=prefix)
+
+    logging.info("===Training===")
+    logging.info(f"===Dataset:{keys}===")
