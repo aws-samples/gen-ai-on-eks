585090bc4f8592efd50a2589805a737f7d3581ec
Gabriel Martini <gabrielb.martini@gmail.com>
2023-08-20 16:27:33 -0300

    feat: ray submit

--- dags/train.py
+++ dags/train.py
@@ -1,10 +1,9 @@
+import logging
+from airflow.hooks.S3_hook import S3Hook
+
+
 def train(**kwargs):
-    import logging
-    from airflow.hooks.S3_hook import S3Hook
-    import ray
-    from ray.train.torch import TorchTrainer
-    from ray.air.config import ScalingConfig, RunConfig
-    from ray.tune import SyncConfig
+    from ray.job_submission import JobSubmissionClient
 
     s3_hook = S3Hook(aws_conn_id="s3_connection")
     bucket = kwargs["bucket"]
@@ -14,3 +13,18 @@ def train(**kwargs):
 
     logging.info("===Training===")
     logging.info(f"===Dataset:{keys[1]}===")
+
+    ray_client = JobSubmissionClient(
+        "http://ray-cluster-kuberay-head-svc.ray-cluster.svc.cluster.local:10001"
+    )
+
+    ray_training = (
+        "rm -rf fm-ops-eks && git clone -b feat/nvidia_gpu_operator https://github.com/lusoal/fm-ops-eks || true;"
+        "chmod +x fm-ops-eks/scripts/train_llm.py && python fm-ops-eks/scripts/train_llm.py"
+    )
+
+    submission_id = ray_client.submit_job(
+        entrypoint=ray_training,
+    )
+
+    logging.info(f"===Submission ID:{submission_id}===")
---
+++ scripts/train_llm.py
@@ -0,0 +1,3 @@
+import logging
+
+logging.info("===Ray log training test===")
