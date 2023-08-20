309398d29faa4deb28820dc57374dad9aa0d01c5
Gabriel Martini <gabrielb.martini@gmail.com>
2023-08-20 17:25:20 -0300

    feat: xgboost example

--- scripts/train_llm.py
+++ scripts/train_llm.py
@@ -1,6 +1,21 @@
 import logging
 import ray
 import argparse
+from ray.data.preprocessors import StandardScaler
+from ray.air.config import ScalingConfig
+from ray.train.xgboost import XGBoostTrainer
+
+
+def prepare_dataset():
+    dataset = ray.data.read_csv(
+        "s3://anonymous@air-example-data/breast_cancer.csv"
+    )
+
+    train_dataset, valid_dataset = dataset.train_test_split(test_size=0.3)
+    test_dataset = valid_dataset.drop_columns(cols=["target"])
+
+    return train_dataset, valid_dataset, test_dataset
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -12,9 +27,30 @@ if __name__ == "__main__":
     )
     args, _ = parser.parse_known_args()
 
-    logging.info("===Ray training===")
-    logging.info(f"===Workers: {args.num_workers}===")
-
     ray.init(address="auto")
 
+    logging.info("===Ray training===")
+    logging.info(f"===Workers: {args.num_workers}===")
     logging.info(ray.cluster_resources())
+
+    train_dataset, valid_dataset, test_dataset = prepare_dataset()
+    preprocessor = StandardScaler(columns=["mean radius", "mean texture"])
+
+    trainer = XGBoostTrainer(
+        scaling_config=ScalingConfig(
+            num_workers=args.num_workers,
+            use_gpu=False,
+            _max_cpu_fraction_per_node=0.9,
+        ),
+        label_column="target",
+        num_boost_round=20,
+        params={
+            "objective": "binary:logistic",
+            "eval_metric": ["logloss", "error"],
+        },
+        datasets={"train": train_dataset, "valid": valid_dataset},
+        preprocessor=preprocessor,
+    )
+
+    model = trainer.fit()
+    logging.info(model.metrics)
